af896972c0f43b595b7aead489eca232f32b633c
BenjaminSipe <benjaminmsipe@student.cofo.edu>
2021-03-27 11:46:50 -0500

    Added basic get and reworked by name

--- routes/case.js
+++ routes/case.js
@@ -1,60 +1,81 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const { MongoClient } = require("mongodb");
 
-const url = 'mongodb://127.0.0.1:27017/';
+const url = "mongodb://127.0.0.1:27017/";
 var client;
-const dbName = 'test';
-const collectionName = "Student"
+const dbName = "test";
+const collectionName = "Student";
 async function getCollection() {
-  client = new MongoClient(url,{ useUnifiedTopology: true });
+  client = new MongoClient(url, { useUnifiedTopology: true });
   return (await client.connect()).db(dbName).collection(collectionName);
 }
 
-
-router.route('/case/by/name/:name')
-  .get(async (req, res) => {
-      var query;
-      var collection = await getCollection();
-      if (req.params.name) {
-        query = {name : req.params.name, doc:{$exists:true}}
-        const person = await collection.findOne(query);
-        await client.close();
-        if (person) {
-          res.send(person);
-        } else {
-          res.send({err:"No Active Case under name " + req.params.name})
-        }
-      } else {
-        res.send({err:"Could not find Parameter Name"})
-      }
-      
-
-})
-router.post('/case', async (req, res)=> {
-    var collection = await getCollection();
-    const response = await collection.insertOne(req.body);
+router.get("/by/name/:name", async (req, res) => {
+  var query;
+  var collection = await getCollection();
+  if (req.params.name) {
+    query = { name: req.params.name, doc: { $exists: false } };
+    const person = await collection.findOne(query);
     await client.close();
-    if (response) {
-      res.send({"i_id":response.ops[0]["_id"]});
+    if (person) {
+      res.send(person);
     } else {
-      res.send({err:"Ooof"})
+      res.send({ err: "No Active Case under name " + req.params.name });
     }
-    });
+  } else {
+    res.send({ err: "Could not find Parameter Name" });
+  }
+});
 
-router.get("/case/all", async (req, res) => {
+router.get("/:id", async (req, res) => {
+  var query;
+  var collection = await getCollection();
+  if (req.params.name) {
+    query = { _id: req.params.id, doc: { $exists: false } };
+    const person = await collection.findOne(query);
+    await client.close();
+    if (person) {
+      res.send(person);
+    } else {
+      res.send({ err: "No Active Case under id " + req.params.id });
+    }
+  } else {
+    res.send({ err: "Could not find Parameter ID" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  var collection = await getCollection();
+  const response = await collection.insertOne(req.body);
+  await client.close();
+  if (response) {
+    res.send({ _id: response.ops[0]["_id"] });
+  } else {
+    res.send({ err: "Ooof" });
+  }
+});
+
+router.get("/all", async (req, res) => {
   try {
-    var arr = []
+    var arr = [];
     const collection = await getCollection();
-    const query = { };
+    const query = { doc: { $exists: false } };
     const cursor = await collection.find(query);
-    await cursor.forEach((elem) => arr.push(elem))
+    await cursor.forEach((elem) => arr.push(elem));
     res.send(arr);
   } finally {
-    await client.close(); 
+    await client.close();
   }
-})
+});
+
+module.exports = express.Router().use("/case", router);
+
+/*
+Tuesday : Class time React, --
+Tuesday : Evening: 8-10:30 Status Report
+Thursday : Class time React/Status meeting,
+Friday : 2:30-5 React--
 
 
-
-module.exports = router;
+*/
--- routes/contact.js
+++ routes/contact.js
@@ -1,17 +1,72 @@
 var express = require("express");
-var router = express.Router()
+var router = express.Router();
+const { MongoClient } = require("mongodb");
 
+const url = "mongodb://127.0.0.1:27017/";
+var client;
+const dbName = "test";
+const collectionName = "Student";
+async function getCollection() {
+  client = new MongoClient(url, { useUnifiedTopology: true });
+  return (await client.connect()).db(dbName).collection(collectionName);
+}
 
-router.route("/contact").get((req, res) => {
-    res.send("contact get")
-}).post((req,res) => {
-    res.send("contact post")
-})
-
-// This is a comment...
-router.get("/contact/all", (req, res) => {
-    res.send("Contact get all")
+router.get("/by/name/:name", async (req, res) => {
+  var query;
+  var collection = await getCollection();
+  if (req.params.name) {
+    query = { name: req.params.name, doc: { $exists: true } };
+    const person = await collection.findOne(query);
+    await client.close();
+    if (person) {
+      res.send(person);
+    } else {
+      res.send({ err: "No Active Case under name " + req.params.name });
+    }
+  } else {
+    res.send({ err: "Could not find Parameter Name" });
+  }
 });
 
+router.get("/:id", async (req, res) => {
+  var query;
+  var collection = await getCollection();
+  if (req.params.id) {
+    query = { _id: req.params.id, doc: { $exists: true } };
+    const person = await collection.findOne(query);
+    await client.close();
+    if (person) {
+      res.send(person);
+    } else {
+      res.send({ err: "No Active Case under ID " + req.params.id });
+    }
+  } else {
+    res.send({ err: "Could not find Parameter ID" });
+  }
+});
 
-module.exports = router;
+router.post("/", async (req, res) => {
+  var collection = await getCollection();
+  const response = await collection.insertOne(req.body);
+  await client.close();
+  if (response) {
+    res.send({ _id: response.ops[0]["_id"] });
+  } else {
+    res.send({ err: "Ooof" });
+  }
+});
+
+router.get("/all", async (req, res) => {
+  try {
+    var arr = [];
+    const collection = await getCollection();
+    const query = { doc: { $exists: true } };
+    const cursor = await collection.find(query);
+    await cursor.forEach((elem) => arr.push(elem));
+    res.send(arr);
+  } finally {
+    await client.close();
+  }
+});
+
+module.exports = express.Router().use("/contact", router);
