0c63f98b5b8bbe58795277722e175e0e7fab2b3b
BenjaminSipe <benjaminmsipe@student.cofo.edu>
2021-04-21 23:50:34 -0500

    search filters out deleted queries, added delete

--- routes/contact.js
+++ routes/contact.js
@@ -9,6 +9,7 @@ router.get("/by/name/:name", async (req, res) => {
       const query = {
         name: req.params.name,
         status: "Exposed",
+        deleted: { $exists: false },
       };
       const person = await collection.findOne(query);
       if (person) {
@@ -31,6 +32,7 @@ router.get("/:id", async (req, res, next) => {
         var query = {
           _id: ObjectID(req.params.id),
           status: "Exposed",
+          deleted: { $exists: false },
         };
         const person = await collection.findOne(query);
         if (person) {
@@ -45,21 +47,57 @@ router.get("/:id", async (req, res, next) => {
   });
 });
 
-router.post("/", async (req, res) => {
+router.delete("/:id", async (req, res) => {
+  if (req.params.id) {
+    await getCollection(async (collection) => {
+      query = {
+        _id: ObjectID(req.params.id),
+        status: "Exposed",
+      };
+      const person = await collection.updateOne(query, {
+        $set: { deleted: true },
+      });
+      if (person.modifiedCount === 1) {
+        res.send({ message: "successfully deleted contact" });
+      } else if (person.matchedCount > 0 && person.modifiedCount === 0) {
+        res.status(202).send({ err: "Patient already marked as deleted" });
+      } else {
+        res.status(400).send({ err: "Unable to find patient" });
+      }
+    });
+  } else {
+    res.status(400).send({ err: "Could not find Parameter ID" });
+  }
+});
+router.get("/past/all", (req, res) => {
+  getCollection(async (collection) => {
+    const query = {
+      status: "Past",
+      deleted: { $exists: false },
+    };
+    let arr = [];
+    const cursor = await collection.find(query);
+    await cursor.forEach((elem) => arr.push(elem));
+    res.send(arr);
+  });
+});
+router.post("/:id", async (req, res) => {
   getCollection(async (collection) => {
     // let { doc, dob } = req.body;
-    let { _id, doc, dob, ...query } = { ...req.body, status: "Exposed" };
+    let { doc, dob, ...query } = { ...req.body, status: "Exposed" };
     if (doc) {
       query.doc = new Date(doc);
     }
     if (dob) {
       query.dob = new Date(dob);
     }
-    if (_id) {
+    if (!req.params.id) {
+      console.log("is this the error that I'm getting?");
+      console.log(req.params.id);
       res.status(400).send({ err: "no id provided" });
     } else {
       var response = await collection.updateOne(
-        { _id: ObjectID(_id) },
+        { _id: ObjectID(req.params.id) },
         { $set: query }
       );
       // console.log(response);
@@ -74,7 +112,7 @@ router.post("/", async (req, res) => {
 
 router.get("/all", async (req, res) => {
   await getCollection(async (collection) => {
-    const query = { status: "Exposed" };
+    const query = { status: "Exposed", deleted: { $exists: false } };
     var arr = [];
     const cursor = await collection.find(query);
     await cursor.forEach((elem) => arr.push(elem));
